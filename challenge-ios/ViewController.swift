9bc03c4819ec62736081ae5f73ce142f8d463b04
Vladislav Ternovskiy <vladislav.ternovskiy@yanpix.com>
2018-05-07 15:54:10 +0300

    Initial Commit

---
+++ challenge-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  challenge-ios
+//
+//  Created by Vladislav Ternovskiy on 07.05.2018.
+//  Copyright © 2018 Vladislav Ternovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
